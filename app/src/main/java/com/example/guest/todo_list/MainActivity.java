f0d516206208d56e79169dda309952e85fccc876
maggieoneill <margaretanneoneill@gmail.com>
2015-10-26 11:05:06 -0700

    initial commit

---
+++ app/src/main/java/com/example/guest/todo_list/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.guest.todo_list;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+
+    private ArrayList<String> mTasks;
+    private Button mNewTaskButton;
+    private EditText mNewTaskText;
+    private ArrayAdapter<String> mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNewTaskButton = (Button) findViewById(R.id.newTaskButton);
+        mNewTaskText = (EditText) findViewById(R.id.newTask);
+        mTasks = new ArrayList<>();
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTasks);
+            setListAdapter(mAdapter);
+
+        mNewTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTask();
+            }
+        });
+    }
+
+    private void addTask() {
+        String newTask = mNewTaskText.getText().toString();
+        mTasks.add(newTask);
+        mAdapter.notifyDataSetChanged();
+    }
+
+}
